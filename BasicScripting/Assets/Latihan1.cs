9c78978d895dbba6566a7334e10e7bb7770c0170
rezaviant <71118175+rezaviant@users.noreply.github.com>
2020-10-13 21:54:11 +0700

    update pertemuan5

---
+++ BasicScripting/Assets/Latihan1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Saya Reza adalah Game Developer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
